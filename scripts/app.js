5f14d13501bf91e9b39f73cc2ba1be91312b5bd9
Ejup Pllashniku <ejup.pllashniku@gmail.com>
2024-07-19 14:43:11 +0200

    Target elements with JS and retrieve values

--- scripts/app.js
+++ scripts/app.js
@@ -13,4 +13,26 @@ class Car extends Vehicle {
     super(make, model, year);
     this.doors = doors;
   }
+
+  getDetails() {
+    return `${this.getDetails()} with ${this.doors} doors`;
+  }
 }
+
+const vehicleForm = document.getElementById("vehicleForm");
+const vehicleList = document.getElementById("vehicleList");
+const filterMake = document.getElementById("filterMake");
+const filterYear = document.getElementById("filterYear");
+
+let vehicle = [];
+
+vehicleForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const make = document.getElementById("make").value;
+  const model = document.getElementById("model").value;
+  const year = document.getElementById("year").value;
+  const doors = document.getElementById("doors").value;
+
+  console.log("test:", make, model, year, doors);
+});
